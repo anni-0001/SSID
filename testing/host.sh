a3128a10fa3048b89e96c88ff42252b6c7fdbbc7
annika <amc1100@gmail.com>
2022-11-18 12:51:10 -0500

    tmuxinator session

--- testing/host.sh
+++ testing/host.sh
@@ -1,12 +1,25 @@
 #!/bin/bash 
 
+port=22; ssh -A -t -p $port root@dev2 ssh -A -t -p $port root@dev3 ssh -A -t -p $port root@dev4  'bash -s'<<EOF 
+        echo "sleeping for 5s"
+        sleep 5
+        pwd
+        echo "sleeping for 5s"
 
-echo '$HOSTNAME'
+        sleep 5
+        env
+        echo "sleeping for 5s"
+        sleep 5
+        seq 10
+EOF      
+echo hi
 
-name=dev
-num=1
-hostnumber=${HOSTNAME:3}
-num2=$((num+1))
-echo $num2
-echo $name$num
+# echo '$HOSTNAME'
+
+# name=dev
+# num=1
+# hostnumber=${HOSTNAME:3}
+# num2=$((num+1))
+# echo $num2
+# echo $name$num
 
