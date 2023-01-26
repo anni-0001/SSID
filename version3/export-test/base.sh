b070bbc6286d08803801abeee98804353c77bf91
annika <amc1100@gmail.com>
2023-01-26 13:48:57 -0500

    working 1 experiemnt

--- version3/export-test/base.sh
+++ version3/export-test/base.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
+
+
+
+# experiment_num=
+
+
 echo $var
 
 export var2=5
+# export tester=1
+
+export HELLO=$(($HELLO+1))
+
+# tester=$[tester+1]
+export tester1=$[tester1+1]
+
+# echo "tester=$tester"
+echo $tester1
+echo $HELLO
