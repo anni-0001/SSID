ff0c6252a17fdc834c332d80909df13111dbf8a5
☆ Anni ☆ <amc1100@rit.edu>
2023-02-23 14:08:52 -0500

    updating paths

--- SSH_TCP_MODEL/docker-internal.sh
+++ SSH_TCP_MODEL/docker-internal.sh
@@ -3,8 +3,8 @@
 # hard-coded params
 FALL_BACK_SCAN_TIME=50
 RT_DIR="/purple/SSH_TCP_MODEL"
-TCP_DIR="/purple/tcpdump"
-experiment_num=$(cat /purple/version3/round.txt)
+TCP_DIR="/purple/SSH_TCP_MODEL/tcpdump"
+experiment_num=$(cat /purple/SSH_TCP_MODEL/round.txt)
 
 if [ $2 ]
 then
@@ -13,7 +13,7 @@ else
     scan_time=$FALL_BACK_SCAN_TIME   # default scantime
 fi
 
-dev_num=$(cat /purple/version3/dev-num.txt)
+dev_num=$(cat /purple/SSH_TCP_MODEL/dev-num.txt)
 echo $dev_num
 
 service ssh restart
