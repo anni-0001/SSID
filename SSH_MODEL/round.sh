3c78b7bd8fce09ef39413460e25555b8300c3ce4
Annika Clarke <amc1100@rit.edu>
2023-05-14 20:02:05 -0400

    reset head to fix repo

--- SSH_MODEL/round.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-var=$(cat round.txt)
-echo $var
--- SSH_MODEL/sshtest.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-devices=4
-sshtunnel=" "
-for ((i=2; i<=devices-1; i++)); do
-    sshtunnel+="ssh -A -t -p $port root@dev$i "
-done 
